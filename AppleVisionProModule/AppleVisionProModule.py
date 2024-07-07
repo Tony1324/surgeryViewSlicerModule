8ab9069d7b481c2bad6b643a9f9b0a08ba7718fd
Tony1324 <61944932+Tony1324@users.noreply.github.com>
2024-07-07 15:30:13 -0400

    Initial UI, pyigtl integration

--- AppleVisionProModule/AppleVisionProModule.py
+++ AppleVisionProModule/AppleVisionProModule.py
@@ -9,14 +9,11 @@ from slicer.i18n import tr as _
 from slicer.i18n import translate
 from slicer.ScriptedLoadableModule import *
 from slicer.util import VTKObservationMixin
-from slicer.parameterNodeWrapper import (
-    parameterNodeWrapper,
-    WithinRange,
-)
 import qt
 from slicer import vtkMRMLScalarVolumeNode
-# import pyigtl
-
+import pyigtl
+from time import sleep
+import threading
 
 #
 # AppleVisionProModule
@@ -47,88 +44,6 @@ This file was originally developed by Jean-Christophe Fillion-Robin, Kitware Inc
 and Steve Pieper, Isomics, Inc. and was partially funded by NIH grant 3P41RR013218-12S1.
 """)
 
-        # Additional initialization step after application startup is complete
-        slicer.app.connect("startupCompleted()", registerSampleData)
-
-
-#
-# Register sample data sets in Sample Data module
-#
-
-
-def registerSampleData():
-    """Add data sets to Sample Data module."""
-    # It is always recommended to provide sample data for users to make it easy to try the module,
-    # but if no sample data is available then this method (and associated startupCompeted signal connection) can be removed.
-
-    import SampleData
-
-    iconsPath = os.path.join(os.path.dirname(__file__), "Resources/Icons")
-
-    # To ensure that the source code repository remains small (can be downloaded and installed quickly)
-    # it is recommended to store data sets that are larger than a few MB in a Github release.
-
-    # AppleVisionProModule1
-    SampleData.SampleDataLogic.registerCustomSampleDataSource(
-        # Category and sample name displayed in Sample Data module
-        category="AppleVisionProModule",
-        sampleName="AppleVisionProModule1",
-        # Thumbnail should have size of approximately 260x280 pixels and stored in Resources/Icons folder.
-        # It can be created by Screen Capture module, "Capture all views" option enabled, "Number of images" set to "Single".
-        thumbnailFileName=os.path.join(iconsPath, "AppleVisionProModule1.png"),
-        # Download URL and target file name
-        uris="https://github.com/Slicer/SlicerTestingData/releases/download/SHA256/998cb522173839c78657f4bc0ea907cea09fd04e44601f17c82ea27927937b95",
-        fileNames="AppleVisionProModule1.nrrd",
-        # Checksum to ensure file integrity. Can be computed by this command:
-        #  import hashlib; print(hashlib.sha256(open(filename, "rb").read()).hexdigest())
-        checksums="SHA256:998cb522173839c78657f4bc0ea907cea09fd04e44601f17c82ea27927937b95",
-        # This node name will be used when the data set is loaded
-        nodeNames="AppleVisionProModule1",
-    )
-
-    # AppleVisionProModule2
-    SampleData.SampleDataLogic.registerCustomSampleDataSource(
-        # Category and sample name displayed in Sample Data module
-        category="AppleVisionProModule",
-        sampleName="AppleVisionProModule2",
-        thumbnailFileName=os.path.join(iconsPath, "AppleVisionProModule2.png"),
-        # Download URL and target file name
-        uris="https://github.com/Slicer/SlicerTestingData/releases/download/SHA256/1a64f3f422eb3d1c9b093d1a18da354b13bcf307907c66317e2463ee530b7a97",
-        fileNames="AppleVisionProModule2.nrrd",
-        checksums="SHA256:1a64f3f422eb3d1c9b093d1a18da354b13bcf307907c66317e2463ee530b7a97",
-        # This node name will be used when the data set is loaded
-        nodeNames="AppleVisionProModule2",
-    )
-
-
-#
-# AppleVisionProModuleParameterNode
-#
-
-
-@parameterNodeWrapper
-class AppleVisionProModuleParameterNode:
-    """
-    The parameters needed by module.
-
-    inputVolume - The volume to threshold.
-    imageThreshold - The value at which to threshold the input volume.
-    invertThreshold - If true, will invert the threshold.
-    thresholdedVolume - The output volume that will contain the thresholded volume.
-    invertedVolume - The output volume that will contain the inverted thresholded volume.
-    """
-
-    inputVolume: vtkMRMLScalarVolumeNode
-    imageThreshold: Annotated[float, WithinRange(-100, 500)] = 100
-    invertThreshold: bool = False
-    thresholdedVolume: vtkMRMLScalarVolumeNode
-    invertedVolume: vtkMRMLScalarVolumeNode
-
-
-#
-# AppleVisionProModuleWidget
-#
-
 
 class AppleVisionProModuleWidget(ScriptedLoadableModuleWidget, VTKObservationMixin):
     """Uses ScriptedLoadableModuleWidget base class, available at:
@@ -142,6 +57,7 @@ class AppleVisionProModuleWidget(ScriptedLoadableModuleWidget, VTKObservationMix
         self.logic = None
         self._parameterNode = None
         self._parameterNodeGuiTag = None
+        self.connected = False
 
     def setup(self) -> None:
         """Called when the user opens the module the first time and the widget is initialized."""
@@ -150,24 +66,58 @@ class AppleVisionProModuleWidget(ScriptedLoadableModuleWidget, VTKObservationMix
         # Create main widget and layout
         panelWidget = qt.QWidget()
         layout = qt.QVBoxLayout(panelWidget)
+        # Set scene in MRML widgets
+        self.layout.addWidget(panelWidget)
+
+        connectionContainer = qt.QWidget()
+        connectionLayout = qt.QVBoxLayout(connectionContainer)
+        #set background
+        connectionContainer.setStyleSheet("background-color: #d0d0d0")
+        layout.addWidget(connectionContainer)
+
+        # Status Message
+        self.status_label = qt.QLabel("Status: Not Connected")
+        connectionLayout.addWidget(self.status_label)
+
 
         # IP Address Input
         ip_address_label = qt.QLabel("IP Address:")
         self.ip_address_input = qt.QLineEdit()
         self.ip_address_input.textChanged.connect(self.validateIPAddress)
-        layout.addWidget(ip_address_label)
-        layout.addWidget(self.ip_address_input)
+        self.ip_address_input.setPlaceholderText("Enter IP Address")
+        self.ip_address_input.setStyleSheet("background-color: white")
+        ip_container = qt.QWidget() 
+        ip_container_layout = qt.QHBoxLayout(ip_container)
+        ip_container_layout.addWidget(ip_address_label)
+        ip_container_layout.addWidget(self.ip_address_input)
+        ip_container_layout.setContentsMargins(0,0,0,0)
+        connectionLayout.addWidget(ip_container)
 
         # Connect Button
-        connect_button = qt.QPushButton("Connect")
-        layout.addWidget(connect_button)
+        self.connect_button = qt.QPushButton("Connect")
+        self.connect_button.clicked.connect(self.onConnectButtonClicked)
+        connectionLayout.addWidget(self.connect_button)
 
-        # Status Message
-        status_label = qt.QLabel("Status: Not Connected")
-        layout.addWidget(status_label)
-        # Set scene in MRML widgets
-        self.layout.addWidget(panelWidget)
-        self.panelWidget.setMRMLScene(slicer.mrmlScene)
+        # Volume
+        self.label = qt.QLabel("Select an image volume:")
+        layout.addWidget(self.label)
+        self.volumeSelector = qt.QComboBox()
+        layout.addWidget(self.volumeSelector)
+        self.updateVolumeSelector()
+
+        self.sendVolumeButton = qt.QPushButton("Send Volume")
+        layout.addWidget(self.sendVolumeButton)
+        self.sendVolumeButton.clicked.connect(self.onSendVolumeButtonClicked)
+        self.sendVolumeButton.setEnabled(False)
+
+        #Models
+        self.sendModelsButton = qt.QPushButton("Send All Models")
+        self.sendModelsButton.clicked.connect(self.onSendModelsButtonClicked)
+        self.sendModelsButton.setEnabled(False)
+        layout.addWidget(self.sendModelsButton)
+    
+        # add vertical spacer
+        layout.addStretch(1)
 
         # Create logic class instance
         self.logic = AppleVisionProModuleLogic()
@@ -177,8 +127,22 @@ class AppleVisionProModuleWidget(ScriptedLoadableModuleWidget, VTKObservationMix
         slicer.mrmlScene.AddObserver(slicer.vtkMRMLScene.StartCloseEvent, self.onSceneStartClose)
         slicer.mrmlScene.AddObserver(slicer.vtkMRMLScene.EndCloseEvent, self.onSceneEndClose)
 
-        # Ensure parameter node is initialized (needed for module reload)
-        self.initializeParameterNode()
+    def updateVolumeSelector(self):
+        self.volumeSelector.clear()
+        volumes = slicer.mrmlScene.GetNodesByClass('vtkMRMLScalarVolumeNode')
+        volumes.UnRegister(None)
+        for i in range(volumes.GetNumberOfItems()):
+            volume = volumes.GetItemAsObject(i)
+            self.volumeSelector.addItem(volume.GetName(), volume.GetID())
+
+    def onSendModelsButtonClicked(self):
+        models = slicer.mrmlScene.GetNodesByClass('vtkMRMLModelNode')
+        
+        for i in range(models.GetNumberOfItems()):
+            model = models.GetItemAsObject(i)
+            # Replace this print statement with the actual sending logic
+            self.logic.sendModel(model)
+            print(f"Sending model: {model.GetName()}")
 
     def validateIPAddress(self,string:str) -> None: 
         numerical = ""
@@ -186,88 +150,43 @@ class AppleVisionProModuleWidget(ScriptedLoadableModuleWidget, VTKObservationMix
             if i.isnumeric() or i==".":
                 numerical += i
         self.ip_address_input.setText(numerical)
-        
 
+    def onConnectButtonClicked(self) -> None:
+        self.logic.onConnection = self.onConnection
+        self.logic.initClient(self.ip_address_input.text)
+    
+    def onSendVolumeButtonClicked(self) -> None:
+        volume = slicer.mrmlScene.GetNodeByID(self.volumeSelector.currentData)
+        self.logic.sendImage(volume)
+    
+    def onConnection(self) -> None:
+        self.connected = True
+        self.status_label.setText("Status: CONNECTED")
+        self.connect_button.setEnabled(False)
+        self.sendVolumeButton.setEnabled(True)
+        self.sendModelsButton.setEnabled(True)
+        
     def cleanup(self) -> None:
         """Called when the application closes and the module widget is destroyed."""
-        self.removeObservers()
+        # self.logic.close()
 
     def enter(self) -> None:
         """Called each time the user opens this module."""
         # Make sure parameter node exists and observed
-        self.initializeParameterNode()
+        self.updateVolumeSelector()
 
     def exit(self) -> None:
         """Called each time the user opens a different module."""
-        # Do not react to parameter node changes (GUI will be updated when the user enters into the module)
-        if self._parameterNode:
-            self._parameterNode.disconnectGui(self._parameterNodeGuiTag)
-            self._parameterNodeGuiTag = None
-            self.removeObserver(self._parameterNode, vtk.vtkCommand.ModifiedEvent, self._checkCanApply)
 
     def onSceneStartClose(self, caller, event) -> None:
         """Called just before the scene is closed."""
         # Parameter node will be reset, do not use it anymore
-        self.setParameterNode(None)
 
     def onSceneEndClose(self, caller, event) -> None:
         """Called just after the scene is closed."""
         # If this module is shown while the scene is closed then recreate a new parameter node immediately
-        if self.parent.isEntered:
-            self.initializeParameterNode()
-
-    def initializeParameterNode(self) -> None:
-        """Ensure parameter node exists and observed."""
-        # Parameter node stores all user choices in parameter values, node selections, etc.
-        # so that when the scene is saved and reloaded, these settings are restored.
-
-        self.setParameterNode(self.logic.getParameterNode())
-
-        # Select default input nodes if nothing is selected yet to save a few clicks for the user
-        if not self._parameterNode.inputVolume:
-            firstVolumeNode = slicer.mrmlScene.GetFirstNodeByClass("vtkMRMLScalarVolumeNode")
-            if firstVolumeNode:
-                self._parameterNode.inputVolume = firstVolumeNode
-
-    def setParameterNode(self, inputParameterNode: Optional[AppleVisionProModuleParameterNode]) -> None:
-        """
-        Set and observe parameter node.
-        Observation is needed because when the parameter node is changed then the GUI must be updated immediately.
-        """
-
-        if self._parameterNode:
-            self._parameterNode.disconnectGui(self._parameterNodeGuiTag)
-            self.removeObserver(self._parameterNode, vtk.vtkCommand.ModifiedEvent, self._checkCanApply)
-        self._parameterNode = inputParameterNode
-        if self._parameterNode:
-            # Note: in the .ui file, a Qt dynamic property called "SlicerParameterName" is set on each
-            # ui element that needs connection.
-            self._parameterNodeGuiTag = self._parameterNode.connectGui(self.ui)
-            self.addObserver(self._parameterNode, vtk.vtkCommand.ModifiedEvent, self._checkCanApply)
-            self._checkCanApply()
-
-    def _checkCanApply(self, caller=None, event=None) -> None:
-        if self._parameterNode and self._parameterNode.inputVolume and self._parameterNode.thresholdedVolume:
-            self.ui.applyButton.toolTip = _("Compute output volume")
-            self.ui.applyButton.enabled = True
-        else:
-            self.ui.applyButton.toolTip = _("Select input and output volume nodes")
-            self.ui.applyButton.enabled = False
-
-    def onApplyButton(self) -> None:
-        """Run processing when user clicks "Apply" button."""
-        with slicer.util.tryWithErrorDisplay(_("Failed to compute results."), waitCursor=True):
-            # Compute output
-            self.logic.process(self.ui.inputSelector.currentNode(), self.ui.outputSelector.currentNode(),
-                               self.ui.imageThresholdSliderWidget.value, self.ui.invertOutputCheckBox.checked)
-
-            # Compute inverted output (if needed)
-            if self.ui.invertedOutputSelector.currentNode():
-                # If additional output volume is selected then result with inverted threshold is written there
-                self.logic.process(self.ui.inputSelector.currentNode(), self.ui.invertedOutputSelector.currentNode(),
-                                   self.ui.imageThresholdSliderWidget.value, not self.ui.invertOutputCheckBox.checked, showResult=False)
-
-
+        # close client
+        self.logic.close()
 #
 # AppleVisionProModuleLogic
 #
@@ -286,49 +205,50 @@ class AppleVisionProModuleLogic(ScriptedLoadableModuleLogic):
     def __init__(self) -> None:
         """Called when the logic class is instantiated. Can be used for initializing member variables."""
         ScriptedLoadableModuleLogic.__init__(self)
+        self.client = None
+        self.onConnection = lambda: None
 
-    def getParameterNode(self):
-        return AppleVisionProModuleParameterNode(super().getParameterNode())
+    def initClient(self,ip:str) -> None:
+        self.client = pyigtl.OpenIGTLinkClient(host=ip, port=18944)
+        self.client.start()
+        threading.Thread(target=self._connect).start()
 
-    def process(self,
-                inputVolume: vtkMRMLScalarVolumeNode,
-                outputVolume: vtkMRMLScalarVolumeNode,
-                imageThreshold: float,
-                invert: bool = False,
-                showResult: bool = True) -> None:
-        """
-        Run the processing algorithm.
-        Can be used without GUI widget.
-        :param inputVolume: volume to be thresholded
-        :param outputVolume: thresholding result
-        :param imageThreshold: values above/below this threshold will be set to 0
-        :param invert: if True then values above the threshold will be set to 0, otherwise values below are set to 0
-        :param showResult: show output volume in slice viewers
-        """
-
-        if not inputVolume or not outputVolume:
-            raise ValueError("Input or output volume is invalid")
-
-        import time
-
-        startTime = time.time()
-        logging.info("Processing started")
-
-        # Compute the thresholded output volume using the "Threshold Scalar Volume" CLI module
-        cliParams = {
-            "InputVolume": inputVolume.GetID(),
-            "OutputVolume": outputVolume.GetID(),
-            "ThresholdValue": imageThreshold,
-            "ThresholdType": "Above" if invert else "Below",
-        }
-        cliNode = slicer.cli.run(slicer.modules.thresholdscalarvolume, None, cliParams, wait_for_completion=True, update_display=showResult)
-        # We don't need the CLI module node anymore, remove it to not clutter the scene with it
-        slicer.mrmlScene.RemoveNode(cliNode)
-
-        stopTime = time.time()
-        logging.info(f"Processing completed in {stopTime-startTime:.2f} seconds")
+    def _connect(self) -> None:
+        while not self.client.is_connected():
+            sleep(0.5)
+        self.onConnection()
+        print("Connected to server")
 
 
+    def sendImage(self, volume:vtkMRMLScalarVolumeNode) -> None:
+        # Create a message with 32 bit int image data
+        image = slicer.util.arrayFromVolume(volume)
+        print(image.shape)
+        imageMessage = pyigtl.ImageMessage(image=image, device_name="VisionProModule")
+        self.client.send_message(imageMessage)
+
+    def sendModel(self, model) -> None:
+        # Create a message with 32 bit int image data
+        modelMessage = pyigtl.PolyDataMessage(model.GetPolyData(), device_name="VisionProModule")
+        self.client.send_message(modelMessage)
+    
+    # def sendTransform(self, transform: vtkMRMLLinearTransformNode) -> None:
+    #     # Create a message with 32 bit int image data
+    #     transformMessage = pyigtl.TransformMessage(transform=transform.GetMatrix(), device_name="VisionProModule")
+    #     self.client.send_message(transformMessage)
+    
+    def sendString(self, string: str) -> None:
+        # Create a message with 32 bit int image data
+        stringMessage = pyigtl.StringMessage(string=string, device_name="VisionProModule")
+        self.client.send_message(stringMessage)
+
+    def close(self) -> None:
+        if self.client is not None:
+            self.client.stop()
+            self.client = None
+        
+
+  
 #
 # AppleVisionProModuleTest
 #
